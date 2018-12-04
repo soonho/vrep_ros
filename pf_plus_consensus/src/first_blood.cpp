6e305bcf6db7f097af5acf6ecab56e4328688aca
Soon Ho <george.pereira@ifrr.edu.br>
2018-12-04 16:38:51 -0400

    First blood: primeiro robo configurado

--- pf_plus_consensus/src/first_blood.cpp
+++ pf_plus_consensus/src/first_blood.cpp
@@ -1,12 +1,15 @@
 #include "ros/ros.h"
 #include <cmath>
 #include <vector>
-#include "geometry_msgs/Twist.h"
+#include "geometry_msgs/Quaternion.h"
+#include "nav_msgs/Odometry.h"
 #include "pf_plus_consensus/PotentialField.h"
 
+// representacao de campos potenciais
 struct pfield {
     double x;
     double y;
+    double z;
     double gain;
     double radius;
     double spread;
@@ -16,6 +19,7 @@ struct pfield {
 ros::Publisher pub_p1;
 
 // listeners dos outros p3
+ros::Subscriber sub_p1;
 ros::Subscriber sub_p2;
 ros::Subscriber sub_p3;
 
@@ -26,7 +30,7 @@ pfield pf_p2;
 pfield pf_p3;
 
 // objeto para publicacao
-geometry_msgs::Twist retorno;
+geometry_msgs::Quaternion retorno;
 
 void init() 
 {
@@ -35,34 +39,45 @@ void init()
     goal.gain = 1.0;
     goal.radius = 0.5;
     goal.spread = 0.5;
+
+    pf_p2.gain = 1.0;
+    pf_p2.radius = 0.5;
+    pf_p2.spread = 0.5;
+
+    pf_p3.gain = 1.0;
+    pf_p3.radius = 0.5;
+    pf_p3.spread = 0.5;
 }
 
 // calculo da forca exercida pelo ponto objetivo
-void goalForce()
+pfield attForce(pfield target, pfield robot)
 {
-    double distance = std::pow(std::pow(goal.x-pf_p3_01.x, 2)+std::pow(goal.y-pf_p3_01.y, 2), 0.5);
-    double psi = std::atan2(goal.y-pf_p3_01.y, goal.x-pf_p3_01.x);
+    pfield temp;
+    double distance = std::pow(std::pow(target.x-robot.x, 2)+std::pow(target.y-robot.y, 2), 0.5);
+    double psi = std::atan2(target.y-robot.y, target.x-robot.x);
     double deltaX, deltaY;
-    if (distance < goal.radius) {
+    if (distance < target.radius) {
         deltaX = 0;
         deltaY = 0;
-    } else if (distance <= goal.spread + goal.radius) {
-        deltaX = goal.gain * (distance - goal.radius) * std::cos(psi);
-        deltaY = goal.gain * (distance - goal.radius) * std::sin(psi);
+    } else if (distance <= target.spread + target.radius) {
+        deltaX = target.gain * (distance - target.radius) * std::cos(psi);
+        deltaY = target.gain * (distance - target.radius) * std::sin(psi);
     } else {
-        deltaX = goal.gain * goal.spread * std::cos(psi);
-        deltaY = goal.gain * goal.spread * std::sin(psi);
+        deltaX = target.gain * target.spread * std::cos(psi);
+        deltaY = target.gain * target.spread * std::sin(psi);
     }
-    retorno.linear.x += deltaX;
-    retorno.linear.y += deltaY;
-    retorno.linear.z += 0.0;
+    temp.x = deltaX;
+    temp.y = deltaY;
+    temp.z = 0.0;
+    return temp;
 }
 
 // calculo da forca exercida por um obstaculo
-void obstacleForce(pfield obs)
+pfield repForce(pfield obs, pfield robot)
 {
-    double distance = std::pow(std::pow(robotX-obs.x, 2)+std::pow(robotY-obs.y, 2), 0.5);
-    double psi = std::atan2(obs.y-robotY, obs.x-robotX);
+    pfield temp;
+    double distance = std::pow(std::pow(robot.x-obs.x, 2)+std::pow(robot.y-obs.y, 2), 0.5);
+    double psi = std::atan2(obs.y-robot.y, obs.x-robot.x);
     double deltaX, deltaY;
     if (distance > obs.spread + obs.radius) {
         deltaX = 0;
@@ -74,26 +89,68 @@ void obstacleForce(pfield obs)
         deltaX = -copysign(1.0, std::cos(psi)) * 9999;
         deltaY = -copysign(1.0, std::sin(psi)) * 9999;
     }
-    force.linear.x += deltaX;
-    force.linear.y += deltaY;
-    force.linear.z += 0.0;
+    temp.x = deltaX;
+    temp.y = deltaY;
+    temp.z = 0.0;
+    return temp;
+}
+
+// funcao para somar dois campos potenciais
+pfield addPfields(pfield a, pfield b) 
+{
+    pfield temp;
+    temp.x = a.x + b.x;
+    temp.y = a.y + b.y;
+    temp.z = a.z + b.z;
+    return temp;
+}
+
+void robot_Callback(const nav_msgs::Odometry::ConstPtr& msg) 
+{
+    pf_p1.x = (double) msg->pose.pose.position.x;
+    pf_p1.y = (double) msg->pose.pose.position.y;
+    pf_p1.z = (double) msg->pose.pose.position.z;
+
+    pfield temp;
+    temp = addPfields(attForce(goal, pf_p1), repForce(pf_p2, pf_p1));
+    temp = addPfields(temp, repForce(pf_p3, pf_p1));
+
+    retorno.x = temp.x;
+    retorno.y = temp.y;
+    retorno.z = 0.0;
+    retorno.w = 0.0;
+
+    pub_p1.publish(retorno);
+}
+
+void p2_Callback(const nav_msgs::Odometry::ConstPtr& msg) 
+{
+    pf_p2.x = (double) msg->pose.pose.position.x;
+    pf_p2.y = (double) msg->pose.pose.position.y;
+}
+
+void p3_Callback(const nav_msgs::Odometry::ConstPtr& msg) 
+{
+    pf_p3.x = (double) msg->pose.pose.position.x;
+    pf_p3.y = (double) msg->pose.pose.position.y;
 }
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "potential_wizard");
+    ros::init(argc, argv, "first_blood");
     ros::NodeHandle n;
 
-    // ros::ServiceServer service_02 = n.advertiseService("add_obstacle", addObstacle);
-    // ROS_INFO("Obstacle adding service: online.");
+    // iniciando parametros
+    init();
 
-    ros::Subscriber robot_01_listener = n.subscribe("robot_call", 100, robot_Callback);
-    ROS_INFO("Control for robot_01: online.\nMay the Force be with you.");
+    // iniciando subscribers
+    sub_p1 = n.subscribe("/odom_p1", 10, robot_Callback);
+    sub_p2 = n.subscribe("/odom_p2", 10, p2_Callback);
+    sub_p3 = n.subscribe("/odom_p3", 10, p3_Callback);
+    ROS_INFO("Control for robot_01: online. First blood.");
 
-    // inicializando subscribers
-    sub_p1 = n.subscribe<geometry_msgs::Twist>("speed_01", 10);
-    sub_p2 = n.subscribe<geometry_msgs::Twist>("speed_02", 10);
-    sub_p3 = n.subscribe<geometry_msgs::Twist>("speed_03", 10);
+    // inicializando advertisers
+    pub_p1 = n.advertise<geometry_msgs::Quaternion>("/speed_01", 10);
 
     ros::spin();
 
