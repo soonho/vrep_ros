01d95403a6fcfb52b121608bf608d659cd4dda08
soonho <georgerr@hotmail.com>
2020-03-05 22:56:46 -0400

    corrigido codigo do quad

--- ros/pf_plus_consensus/src/quadra_kill.cpp
+++ ros/pf_plus_consensus/src/quadra_kill.cpp
@@ -1,8 +1,14 @@
 #include "ros/ros.h"
-#include <cmath>
+#include "geometry_msgs/Vector3.h"
 #include "geometry_msgs/Quaternion.h"
+#include "tf/transform_datatypes.h"
+#include "tf/LinearMath/Matrix3x3.h"
+#include <cmath>
+#include <math.h>
 #include "nav_msgs/Odometry.h"
 #include "potential_fields.cpp"
+
+using namespace std;
 
 // representacao de campos potenciais
 struct pfield {
@@ -52,6 +58,17 @@ float gain_vz   = 0.15;
 float gain_vyaw = 0.15;
 float target_z = 1.0;
 
+//parametros para limitadores e offsets
+float max_accxy  = 0.15;
+float max_accz   = 0.5;
+float min_accz   = 0.465;
+float max_uyaw   = 0.8;
+float offset_z   = 0.465;
+float offset_yaw = 0.5;
+
+//variaveis para o calculo de saturacao
+float rmax = 0, rx = 0, ry = 0, rz = 0, ryaw = 0, pf_p3_yaw = 0;
+
 void initParams() 
 {
     pf_p2.gain = 1.0;
@@ -65,6 +82,10 @@ void initParams()
     pf_p3.gain = 1.0;
     pf_p3.radius = 1.0;
     pf_p3.spread = 1.0;
+
+    goal.gain = 1.0;
+    goal.radius = 1.0;
+    goal.spread = 1.0;
 }
 
 //leis de controle: omniant1 + omniant2 + quad1 (ganhos dinamicos)
@@ -75,25 +96,54 @@ PotentialField consensus() {
     double uz = gain_z * (target_z   - pf_q1.z)                                                                  - gain_vz * (pf_q1.vz);
     //uyaw = gain_yaw * (0.5 * ((omniant1_yaw - 0.0) - (quad1_yaw - 0.0)) + 0.5 * ((omniant2_yaw - 0.0) - (quad1_yaw - 0.0))) - gain_vyaw * (quad1_vyaw);
 
-    temp.x = ux;// ux * cos(quad1_yaw) + uy * sin(quad1_yaw);
-    temp.y = uy;//-ux * sin(quad1_yaw) + uy * cos(quad1_yaw);
+    temp.x = ux * cos(pf_p3_yaw) + uy * sin(pf_p3_yaw);
+    temp.y = -ux * sin(pf_p3_yaw) + uy * cos(pf_p3_yaw);
     temp.z = uz;
     return temp;
 }
 
 void robot_Callback(const nav_msgs::Odometry::ConstPtr& msg) 
 {
-    pf_p3.x = (double) msg->pose.pose.position.x;
-    pf_p3.y = (double) msg->pose.pose.position.y;
-    pf_p3.z = (double) msg->pose.pose.position.z;
+    pf_q1.x = (double) msg->pose.pose.position.x;
+    pf_q1.y = (double) msg->pose.pose.position.y;
+    pf_q1.z = (double) msg->pose.pose.position.z;
+    pf_p3_yaw = msg->pose.pose.orientation.w;
+/*
+    retorno.x = (double) msg->pose.pose.orientation.x;
+    retorno.y = (double) msg->pose.pose.orientation.y;
+    retorno.z = (double) msg->pose.pose.orientation.z;
+    retorno.w = (double) msg->pose.pose.orientation.w;
 
-    PotentialField temp = consensus();
-    temp.add(temp.repForce(pf_o1, pf_q1));
-    temp.add(temp.repForce(pf_o2, pf_q1));
-    temp.add(temp.repForce(pf_o3, pf_q1));
+    // the incoming geometry_msgs::Quaternion is transformed to a tf::Quaterion
+    tf::Quaternion quat;
+    tf::quaternionMsgToTF(retorno, quat);
 
-    retorno.x = temp.x;
-    retorno.y = temp.y;
+    // the tf::Quaternion has a method to acess roll pitch and yaw
+    double roll, pitch, yaw;
+    tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
+    pf_p3_yaw = yaw;
+*/
+    //PotentialField temp = consensus();
+    //temp.add(temp.repForce(pf_o1, pf_q1));
+    //temp.add(temp.repForce(pf_o2, pf_q1));
+    //temp.add(temp.repForce(pf_o3, pf_q1));
+
+    PotentialField temp;
+    temp.add(temp.attForce(goal, pf_q1));
+
+    //saturacao
+    rx = abs(temp.x / max_accxy);
+    ry = abs(temp.y / max_accxy);
+
+    rmax = max(rx, ry);
+
+    if (rmax > 1) {
+        rx = temp.x / rmax;
+        ry = temp.y / rmax;
+    }
+
+    retorno.x = rx;
+    retorno.y = ry;
     retorno.z = 0.0;
     retorno.w = 0.0;
 
@@ -141,7 +191,7 @@ int main(int argc, char **argv)
     sub_p2 = n.subscribe("/odom_p2", 10, p2_Callback);
     sub_p3 = n.subscribe("/odom_p3", 10, p3_Callback);
     sub_q1 = n.subscribe("/odom_q1", 10, robot_Callback);
-    sub_q1 = n.subscribe("/odom_r1", 10, r1_Callback);
+    sub_r1 = n.subscribe("/odom_r1", 10, r1_Callback);
     ROS_INFO("Control for quad_01: online. Quadra kill.");
 
     // inicializando advertisers
